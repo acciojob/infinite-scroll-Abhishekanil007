ba368bebb3899519f0f7a9eaa883e9cad5aac95c
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-10-30 11:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+let infilist = document.getElementById('infi-list');
+function addmore(){
+	for(let i=1; i<=2; i++)
+		{
+			let listItems = document.createElement('li');
+			listItems.textContent = 'Item '+ (infilist.children.length + i);
+			infilist.appendChild(listItems);
+		}
+}
 
+infilist.addEventListener('scroll', function(){
+	if(infilist.scrollTop + infilist.clientHeight >= infilist.scrollHeight){
+		addmore();
+	}
+});
+
+addmore();
